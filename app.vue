a50b6913e2d3e8394ff0e4459a45aedde00da69a
KamenSentai <cvt.alain@gmail.com>
2022-07-20 11:43:18 +0200

    feat(): update

--- app.vue
+++ app.vue
@@ -1,60 +1,60 @@
 <template>
-  <UtilsNotificationBanner
+  <!-- <UtilsNotificationBanner
     v-if="notificationsBanner"
     v-bind="notificationsBanner"
-  />
-  <PartialsNavigationHeader />
+  /> -->
+  <!-- <PartialsNavigationHeader /> -->
   <!-- <NuxtPage /> -->
   <transition name="fade">
-    <UtilsOverlay v-if="!!currentPopup.component" />
+    <!-- <UtilsOverlay v-if="!!currentPopup.component" /> -->
   </transition>
-  <transition name="popup-bounce">
+  <!-- <transition name="popup-bounce">
     <component
       :is="currentPopup.component"
       v-if="!!currentPopup.component"
       v-bind="currentPopup.params"
       v-on="currentPopup.events"
     />
-  </transition>
-  <UtilsNotificationToast
+  </transition> -->
+  <!-- <UtilsNotificationToast
     v-for="(notificationToast, i) in notificationsToast"
     :key="`notificationToast-${i}`"
     v-bind="notificationToast"
-  />
+  /> -->
 </template>
 
 <script setup lang="ts">
-import type { NotificationBannerProps } from '@/composables/useNotificationBanner'
-import type { NotificationToastProps } from '@/composables/useNotificationToast'
-import { useDeviceStore } from '@/stores/device'
-import { useProposalsStore } from '@/stores/proposals'
-import BrowserDetector from '@/assets/scripts/detectors/BrowserDetector.class'
-import DeviceDetector from '@/assets/scripts/detectors/DeviceDetector.class'
+// import type { NotificationBannerProps } from '@/composables/useNotificationBanner'
+// import type { NotificationToastProps } from '@/composables/useNotificationToast'
+// import { useDeviceStore } from '@/stores/device'
+// import { useProposalsStore } from '@/stores/proposals'
+// import BrowserDetector from '@/assets/scripts/detectors/BrowserDetector.class'
+// import DeviceDetector from '@/assets/scripts/detectors/DeviceDetector.class'
 
-const notificationsBanner = useState<NotificationBannerProps>('notifications-banner')
-const notificationsToast = useState<NotificationToastProps[]>('notifications-toast')
+// const notificationsBanner = useState<NotificationBannerProps>('notifications-banner')
+// const notificationsToast = useState<NotificationToastProps[]>('notifications-toast')
 
-const { setBrowser, setDevice } = useDeviceStore()
-const { fetchProposals } = useProposalsStore()
-const { bus, events } = useEventsBus()
-const { listenIconex } = useIconexListener()
-const {
-  currentPopup,
-  POPUP_CLOSE_CURRENT,
-  POPUP_HANDLE_GUARD,
-  POPUP_CALL_ACTION,
-} = usePopupMethods()
+// const { setBrowser, setDevice } = useDeviceStore()
+// const { fetchProposals } = useProposalsStore()
+// const { bus, events } = useEventsBus()
+// const { listenIconex } = useIconexListener()
+// const {
+//   currentPopup,
+//   POPUP_CLOSE_CURRENT,
+//   POPUP_HANDLE_GUARD,
+//   POPUP_CALL_ACTION,
+// } = usePopupMethods()
 
-watch(() => bus.value.get(events.POPUP_CLOSE), POPUP_CLOSE_CURRENT)
-watch(() => bus.value.get(events.POPUP_GUARD), POPUP_HANDLE_GUARD)
-watch(() => bus.value.get(events.POPUP_ACTION), POPUP_CALL_ACTION)
+// watch(() => bus.value.get(events.POPUP_CLOSE), POPUP_CLOSE_CURRENT)
+// watch(() => bus.value.get(events.POPUP_GUARD), POPUP_HANDLE_GUARD)
+// watch(() => bus.value.get(events.POPUP_ACTION), POPUP_CALL_ACTION)
 
-onBeforeMount(async () => {
-  const { browser } = new BrowserDetector()
-  const checks = new DeviceDetector()
-  setBrowser(browser.toLowerCase())
-  setDevice(checks)
-  listenIconex()
-  await fetchProposals()
-})
+// onBeforeMount(async () => {
+//   const { browser } = new BrowserDetector()
+//   const checks = new DeviceDetector()
+//   setBrowser(browser.toLowerCase())
+//   setDevice(checks)
+//   listenIconex()
+//   await fetchProposals()
+// })
 </script>
